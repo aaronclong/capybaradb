cfd39d841718c45f09429b0586d786f26215d4a5
Aaron Long <aaron.long.c@gmail.com>
2017-12-27 22:17:57 -0500

    Started on lexer for parser

---
+++ src/query_processor/lexicon.go
@@ -0,0 +1,33 @@
+package query_processor
+
+import "strings"
+
+type Clause byte
+
+const (
+	SELECT Clause = iota
+	UPDATE
+	INSERT
+	FROM
+	WHERE
+	GROUP_BY
+	EQUALS
+	NOT_A_CLAUSE
+)
+
+func EvaluateWord(word string) Clause {
+	switch strings.ToUpper(word) {
+	case "SELECT":
+		return SELECT
+	case "FROM":
+		return FROM
+	case "WHERE":
+		return WHERE
+	case "GROUP BY":
+		return GROUP_BY
+	case "=":
+		return EQUALS
+	default:
+		return NOT_A_CLAUSE
+	}
+}
