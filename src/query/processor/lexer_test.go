464262441e2cc17478091f36143d3c81e806f84b
Aaron Long <aaron.long.c@gmail.com>
2017-12-31 15:52:24 -0500

    Got some more of dem good, good test

--- src/query/processor/lexer_test.go
+++ src/query/processor/lexer_test.go
@@ -8,11 +8,29 @@ func TestLexerHappyPath(t *testing.T) {
 	const sqlString = "SELECT * FROM db;"
 	var lexedValues = Lexer(sqlString)
 	selectClause := lexedValues[Select]
-	if selectClause.Contains("*") != true {
+	if !selectClause.Contains("*") {
 		t.Errorf("Lexer Happy Path missing SELECT *")
 	}
 	fromClause := lexedValues[From]
-	if fromClause.Contains("db") != true {
+	if !fromClause.Contains("db") {
 		t.Errorf("Lexer Happy Path missing FROM db")
+	}
+}
+
+func TestLexerComplicatedQuery(t *testing.T) {
+	const sqlString = "SELECT Table, Owner FROM db WHERE table.name = 'aaron';"
+	var lexedValues = Lexer(sqlString)
+	selectClause := lexedValues[Select]
+	if !selectClause.Contains("Table,") && !selectClause.Contains("Owner") {
+		t.Errorf("Lexer Complicated Query missing SELECT clause")
+	}
+	fromClause := lexedValues[From]
+	if !fromClause.Contains("db") {
+		t.Errorf("Lexer Complicated Query missing FROM db")
+	}
+
+	whereClause := lexedValues[Where]
+	if !whereClause.Contains("table.name") && !whereClause.Contains("=") && !whereClause.Contains("'aaron'") {
+		t.Errorf("Lexer Complicated Query missing FROM db")
 	}
 }
