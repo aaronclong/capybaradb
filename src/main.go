7f9a1887f34b4fc3a83af1a7642e6387b2e48796
Aaron Long <aaron.long.c@gmail.com>
2017-12-31 13:24:20 -0500

    Test import into main

--- src/main.go
+++ src/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/aaronclong/capybaradb/src/query/processor"
 )
 
 func main() {
 	fmt.Println("Hello, Gopher!")
+	processor.Lexer("SELECT * FROM 'DB'")
 }
